44f7e2724789426dd63b4c723f6691baa9cd9101
Yomar Miranda <mirandayomar@gmail.com>
2017-11-02 22:06:03 -0500

    Aquí podemos ver como se pueden crear animaciones con Javascript, luego de tener el elemento utilizamos la propiedad 'animate' el cual recibe dos parametros, el primero es un Array de Keyframes y el segundo un objeto con las opciones para la configuración de las animaciones. Hay que tener en cuenta las últimas dos opciones (iterationsStart, endDelay) en la primera se indica donde empieza la animación y solo se permiten valores entre 0 y 1 los cuales representan porcentajes

--- js/animate.js
+++ js/animate.js
@@ -1,1 +1,22 @@
 const $circulo = document.getElementById('circulo');
+
+// element.animate(keyframes = [], options = {})
+$circulo.animate([
+	//from
+	{
+		transform: 'translateX(0)'
+	},
+	//to
+	{
+		transform: 'translateX(500px)'
+	}
+], {
+	duration: 1000,
+	delay: 1000,
+	direction: 'normal',
+	easing: 'linear',
+	iterations: Infinity,
+	fill: 'forwards',
+	iterationsStart: .5,
+	//endDelay: 5000
+})
